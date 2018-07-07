de1f779d5a494bffd10663ae269b73368970ff06
TJ <tonyjsouthern@gmail.com>
2018-07-07 15:41:34 -0400

    Removed for loop in favor of recursive function, added some comments and global variables

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ var sql = require('sequelize');
 var queries = require('./sql.js')
 require('dotenv').config()
 
+// GLOBAL VARIABLES
 // variable to store customer domains
 var domains = [];
+// varable used to store the html after axios call
+var storedHTML
+// varable that stores the current site the iteration is on
+var currentSite
+// counter for recursive function
+var i = 591
+
 
 // config for database
 var config = {
@@ -24,26 +32,80 @@ var config = {
 // connect to the database
 var connection = new sql(config.database, config.userName, config.password, config);
 
-// intitialization function
-function init() {
-  new Promise(function(resolve, reject) {
-    var dataSetOne = queryDbs(queries.one)
-    var dataSetTwo = queryDbs(queries.two)
-    resolve(dataSetOne, dataSetTwo);
-  }).then(function() {
-    console.log(domains)
- })
-}
-
-// calls the init function
-init()
-
 // querys the DB and pushes the results to the global domains varbale
 function queryDbs(query) {
   return connection.query(query).spread(function(results) {
-        for (var i = 0; i < results.length; i++) {
-          domains.push(results[i].domainname)
+    for (var i = 0; i < results.length; i++) {
+      domains.push(results[i].domainname)
     }
     connection.close();
   })
+}
+
+// intitialization function
+function init() {
+  new Promise(function(resolve, reject) {
+    // Runs the first SQL query to gather domains
+    var dataSetOne = queryDbs(queries.one)
+    // Runs the second SQL query to gather domains
+    var dataSetTwo = queryDbs(queries.two)
+    resolve(dataSetOne, dataSetTwo);
+  }).then(function() {
+    console.log("Number of domains to scan: " + domains.length + "\n")
+    // intitializes the recursive loop function
+    loopSites()
+  })
+}
+
+function loopSites() {
+  new Promise(function(resolve, reject) {
+    // set the global variable to the current website being checked
+    currentSite = "http://" + domains[i];
+    // log the current site and number in the domains array
+    console.log(i + " : " + currentSite)
+    // run loadsite function
+    var temp = loadSite(currentSite);
+    // resolve it
+    resolve(temp);
+  }).then(function() {
+    // add a new function that contains all checks and takes a data parameter
+    checkSF(storedHTML, currentSite)
+    if (i == domains.length) {
+      console.log("finished")
+    } else {
+      i++
+      loopSites();
+    }
+  }).catch((err) => {
+    console.log(err);
+    i++
+    loopSites();
+})
+}
+
+function loadSite() {
+  return axios.get(currentSite)
+    .then(response => {
+      storedHTML = response.data
+    })
+    .catch(error => {
+      fs.appendFile('sites/Error-Log.txt', currentSite + " : Error Code - " +  error.code + "\n", (err) => {
+        console.log("An error has occurred loading this website. Check the error.txt file for more information.");
+      });
+    });
+}
+
+// intitializes program
+init()
+
+
+// Tracking Script Checks
+
+function checkSF(data, site) {
+  if (data.indexOf('sf_config') != -1 || data.indexOf('frt(') != -1) {
+    fs.appendFile('sites/SF.txt', site + "\n", (err) => {
+      if (err) throw err;
+      console.log(site + " : Saved - SF");
+    });
+  }
 }
